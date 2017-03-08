d12187ad7da23ceb546fbd55272e9b8ab1e09121
Hao(Leo) Luo <robzai@hotmail.com>
2017-03-08 12:59:17 -0800

    job11b done

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -4,79 +4,80 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mtce extends Application {
 
-	private $items_per_page = 10;
+    private $items_per_page = 10;
 
-	public function index()
-	{
-		$this->page(1);
-	}
+    public function index()
+    {
+        $this->page(1);
+    }
 
-	// Show a single page of todo items
-	private function show_page($tasks)
-	{
-		$role = $this->session->userdata('userrole');
-		$this->data['pagetitle'] = 'TODO List Maintenance ('. $role . ')';
-		
-		// build the task presentation output
-		$result = ''; // start with an empty array		
-		foreach ($tasks as $task)
-		{
-                    if (!empty($task->status)) {
-                        $task->status = $this->statuses->get($task->status)->name;
-                    }
-                    if ($role == ROLE_OWNER) {
-                        $result .= $this->parser->parse('oneitemx', (array) $task, true);
-                    } else {
-                        $result .= $this->parser->parse('oneitem', (array) $task, true);
-                    }
+    // Show a single page of todo items
+    private function show_page($tasks)
+    {
+        $role = $this->session->userdata('userrole');
+        $this->data['pagetitle'] = 'TODO List Maintenance ('. $role . ')';
+
+        // build the task presentation output
+        $result = ''; // start with an empty array		
+        foreach ($tasks as $task)
+        {
+            if (!empty($task->status)) {
+                $task->status = $this->statuses->get($task->status)->name;
+            }
+            if ($role == ROLE_OWNER) {
+                $result .= $this->parser->parse('oneitemx', (array) $task, true);
+            } else {
+                $result .= $this->parser->parse('oneitem', (array) $task, true);
+            }
         }
 
-		$this->data['display_tasks'] = $result;
-        
-		// and then pass them on
-		$this->data['pagebody'] = 'itemlist';
-		$this->render();
-	}
+            $this->data['display_tasks'] = $result;
 
-	// Extract & handle a page of items, defaulting to the beginning
-	function page($num = 1)
-	{
-		$records = $this->tasks->all(); // get all the tasks
-		$tasks = array(); // start with an empty extract
-		// use a foreach loop, because the record indices may not be sequential
-		$index = 0; // where are we in the tasks list
-		$count = 0; // how many items have we added to the extract
-		$start = ($num - 1) * $this->items_per_page;
-		foreach ($records as $task)
-		{
-			if ($index++ >= $start)
-			{
-				$tasks[] = $task;
-				$count++;
-			}
-			if ($count >= $this->items_per_page)
-				break;
-		}
+            // and then pass them on
+            $this->data['pagebody'] = 'itemlist';
+            $this->render();
+    }
 
-                $this->data['pagination'] = $this->pagenav($num);
-                // INSERT next three lines
-                $role = $this->session->userdata('userrole');
-                if ($role == ROLE_OWNER) 
-                        $this->data['pagination'] .= $this->parser->parse('itemadd',[], true);
-                $this->show_page($tasks);
-	}
+    // Extract & handle a page of items, defaulting to the beginning
+    function page($num = 1)
+    {
+            $records = $this->tasks->all(); // get all the tasks
+            $tasks = array(); // start with an empty extract
+            // use a foreach loop, because the record indices may not be sequential
+            $index = 0; // where are we in the tasks list
+            $count = 0; // how many items have we added to the extract
+            $start = ($num - 1) * $this->items_per_page;
+            foreach ($records as $task)
+            {
+                    if ($index++ >= $start)
+                    {
+                            $tasks[] = $task;
+                            $count++;
+                    }
+                    if ($count >= $this->items_per_page)
+                            break;
+            }
 
-	// Build the pagination navbar
-	private function pagenav($num) {
-		$lastpage = ceil($this->tasks->size() / $this->items_per_page);
-		$parms = array(
-			'first' => 1,
-			'previous' => (max($num-1,1)),
-			'next' => min($num+1,$lastpage),
-			'last' => $lastpage
-		);
-		return $this->parser->parse('itemnav',$parms,true);
-	}
+            $this->data['pagination'] = $this->pagenav($num);
+            // INSERT next three lines
+            $role = $this->session->userdata('userrole');
+            if ($role == ROLE_OWNER) 
+                    $this->data['pagination'] .= $this->parser->parse('itemadd',[], true);
+            $this->show_page($tasks);
+    }
+
+    // Build the pagination navbar
+    private function pagenav($num) {
+            $lastpage = ceil($this->tasks->size() / $this->items_per_page);
+            $parms = array(
+                    'first' => 1,
+                    'previous' => (max($num-1,1)),
+                    'next' => min($num+1,$lastpage),
+                    'last' => $lastpage
+            );
+            return $this->parser->parse('itemnav',$parms,true);
+    }
+    
     // Initiate adding a new task
     public function add() {
         $task = $this->tasks->create();
@@ -92,24 +93,53 @@ class Mtce extends Application {
         $this->session->set_userdata('task', $task);
         $this->showit();
     }
+    
     // Render the current DTO
     private function showit() {
         $task = $this->session->userdata('task');
+        
         $this->data['id'] = $task->id;
         foreach ($this->priorities->all() as $record)
         {
             $priparms[$record->id] = $record->name;
         }
+        
+        foreach ($this->sizes->all() as $record)
+        {
+            $sizes[$record->id] = $record->name;
+        }
+        foreach ($this->groups->all() as $record)
+        {
+            $groups[$record->id] = $record->name;
+        }
+        foreach ($this->statuses->all() as $record)
+        {
+            $status[$record->id] = $record->name;
+        }
+        
         $fields = array(
-            'ftask' => makeTextField('Task description', 'task', $task->task, 'Work', "What needs to be done?"),
-            'fpriority' => makeComboBox('Priority', 'priority', $task->priority, $priparms, "How important is this task?"),
-            'zsubmit' => makeSubmitButton('Update the TODO task', "Click on home or <back> if you don't want to change anything!", 'btn-success'),
+            'ftask' => makeTextField('Task description', 'task', $task->task,
+                    'Work', "What needs to be done?"),
+            'fpriority' => makeComboBox('Priority', 'priority', $task->priority,
+                    $priparms, "How important is this task?"),
+            
+            'fsize' => makeComboBox('Size', 'size', $task->size,
+                    $sizes, "What is the size of this task?"),
+            'fgroup' => makeComboBox('Group', 'group', $task->group,
+                    $groups, "Which group is this task belong to?"),
+            'fstatus' => makeComboBox('Status', 'status', $task->status,
+                    $status, "Status of this task?"),
+            
+            'zsubmit' => makeSubmitButton('Update the TODO task',
+                    "Click on home or <back> if you don't want to change anything!",
+                    'btn-success'),
         );
         $this->data = array_merge($this->data, $fields);
 
         $this->data['pagebody'] = 'itemedit';
         $this->render();
     }
+    
     // handle form submission
     public function submit() {
         // setup for validation
@@ -133,10 +163,12 @@ class Mtce extends Application {
         }
         $this->showit();
     }
+    
     public function cancel() {
         $this->session->unset_userdata('task');
         redirect('/mtce');
     }
+    
     function delete() {
         $dto = $this->session->userdata('task');
         $task = $this->tasks->get($dto->id);
